51b8ee856ef26882dcc5307c0b99337d882ba0d1
allisondurham <allison.durham@gmail.com>
2021-04-27 01:45:04 -0400

    bedtime for real

--- exercises.py
+++ exercises.py
@@ -223,7 +223,56 @@ what to do if that error happens goes in the except clause
 
 
 
+
+
 # Chapter 4
+
+# 1. What is []?
+an empty list
+
+# 2. How would you assign the value 'hello' as the third value in a list stored in a variable named spam? (Assume spam contains [2, 4, 6, 8, 10].)
+spam.insert(2,"hello")
+
+# For the following three questions, let’s say spam contains the list ['a', 'b', 'c', 'd'].
+# 3. What does spam[int(int('3' * 2) // 11)] evaluate to?
+d
+
+# 4. What does spam[-1] evaluate to?
+d
+
+# 5. What does spam[:2] evaluate to?
+['a', 'b']
+
+# For the following three questions, let’s say bacon contains the list [3.14, 'cat', 11, 'cat', True].
+# 6. What does bacon.index('cat') evaluate to?
+1
+
+# 7. What does bacon.append(99) make the list value in bacon look like?
+[3.14, 'cat', 11, 'cat', True, 99]
+
+# 8. What does bacon.remove('cat') make the list value in bacon look like?
+[3.14, 11, 'cat', True, 99]
+
+# 9. What are the operators for list concatenation and list replication?
++ for concatenation, and * for replication
+
+# 10. What is the difference between the append() and insert() list methods?
+.append() adds something to the end of a list, and .insert ( , ) adds something to an indexed spot in the list
+
+# 11. What are two ways to remove values from a list?
+.pop() and .remove()
+
+# 12. Name a few ways that list values are similar to string values.
+
+# 13. What is the difference between lists and tuples?
+
+# 14. How do you type the tuple value that has just the integer value 42 in it?
+
+# 15. How can you get the tuple form of a list value? How can you get the list form of a tuple value?
+
+# 16. Variables that “contain” list values don’t actually contain lists directly. What do they contain instead?
+
+# 17. What is the difference between copy.copy() and copy.deepcopy()?
 
 # Chapter 5
 
--- practice.py
+++ practice.py
@@ -1,12 +1,18 @@
-for i in range(1, 11):
-    print(i)
+# for i in range(1, 11):
+#     print(i)
 
-number = 0
-while number < 10 :
-    number = number + 1
-    print(number)
+# number = 0
+# while number < 10 :
+#     number = number + 1
+#     print(number)
 
-import areallyourpetsnamederic
+# spam = ['a', 'b', 'c', 'd']
+# print(spam[:2])
 
-areallyourpetsnamederic.print("pet")
+bacon = [3.14, 'cat', 11, 'cat', True]
+# print(bacon.index('cat'))
+bacon.append(99)
+print(bacon)
 
+bacon.remove("cat")
+print(bacon)
